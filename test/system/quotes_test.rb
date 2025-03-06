8c5fc82085083262c566bef548823a1d08c5c74f
biancacramos <bianca.ramos777@gmail.com>
2025-03-06 17:33:26 -0300

    feat: continue tutorial

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,8 +2,16 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
 
-  setup do
-    @quote = quotes(:first)
+  setup do 
+    login_as users(:accountant)
+    @quote = Quote.ordered.first
+  end
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
   end
 
   test "Creating a new quote" do
@@ -11,29 +19,23 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
     fill_in "Name", with: "Test quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
     assert_selector "h1", text: "Quotes"
     assert_text "Test quote"
   end
 
-  test "showing a quote" do
-    visit quotes_path
-    click_link @quote.Name
-
-    assert_selector "h1", text: @quote.name
-  end
-
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    click_on "Edit quote"
-
+    click_on "Edit", match: :first
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
